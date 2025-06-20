024fcede822f187392151eed994c65704a90bb62
Antar Ibn Shadad <aralkandari@gmail.com>
2025-06-20 05:56:30 -0700

    chatbot added

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,4 +1,5 @@
 const BASE_URL = 'https://localhost:7165/api';
+const CHAT_URL = 'http://127.0.0.1:5000' 
 
 export const API = {
   AUTH: {
@@ -8,4 +9,5 @@ export const API = {
       TIMINGPLAN: `${BASE_URL}/TimingPlans`,
       DEPARTMENTS: `${BASE_URL}/Departments`,
       LOCATIONS: `${BASE_URL}/Locations`,
+      CHAT:`${CHAT_URL}/Chat`
 };
